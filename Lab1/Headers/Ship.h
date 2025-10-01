ee3978c68c75538eef372b049145c5adab0fe158
anzhela <angela.tes3@gmail.com>
2025-10-02 00:03:52 +0300

    Add header Ship

--- Lab1/Headers/Ship.h
+++ Lab1/Headers/Ship.h
@@ -4,7 +4,7 @@
 
 class Ship : public Vehicle {
 public:
-    Ship(const string& name = "Ship", double speed = 40, double fuel = 1800)
+    Ship(const string& name = "Ship", double speed = 35, double fuel = 1800)
         : Vehicle(name, speed, fuel) {}
     double getSpeed(RoadType type) const override {
         return (type == RoadType::WATER) ? m_speed : 0;
