64608046140f88686827750130cd878ee0435184
anzhela <angela.tes3@gmail.com>
2025-10-02 23:38:35 +0300

    Added m_availableVehicles and methods addVehicle and getAvailableVehicles

--- Lab1/Headers/Vertex.h
+++ Lab1/Headers/Vertex.h
@@ -1,11 +1,15 @@
 #pragma once
 
 #include <string>
+#include <vector>
+#include "Vehicle.h"
+
 using namespace std;
 
 class Vertex {
     private:
         string m_name;
+        vector<Vehicle*> m_availableVehicles;
     public:
         Vertex() : m_name() {}
         explicit Vertex(const string& name) : m_name(name) {}
@@ -13,4 +17,7 @@ class Vertex {
 
         void setName(const string& name) { m_name = name; }
         string getName() const { return m_name; }
+
+        void addVehicle(Vehicle* vehicle) { m_availableVehicles.push_back(vehicle); }
+        vector<Vehicle*> getAvailableVehicles() const { return m_availableVehicles; }
 };
