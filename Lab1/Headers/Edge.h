75552e0daf5c5157cead4ab95d5ac532dc9eab17
anzhela <angela.tes3@gmail.com>
2025-10-04 23:55:43 +0300

    Added enum class RoadCharacteristic, methods calculateTravelTime and getWeight

--- Lab1/Headers/Edge.h
+++ Lab1/Headers/Edge.h
@@ -1,13 +1,17 @@
 #pragma once
 
+#include <limits>
 #include <string>
+#include <vector>
 #include "Vertex.h"
 #include "Vehicle.h"
-using namespace std;
+
+enum class RoadType { ROAD, RAIL, AIR, WATER };
+enum class RoadCharacteristic { HIGHWAY, CITY, DIRT_ROAD, PARK, STANDARD };
 
 struct Obstacle {
-    string name;
-    double delayHours;
+    std::string name;
+    double delayHours = 0.0;
 };
 
 class Edge {
@@ -16,20 +20,41 @@ class Edge {
         Vertex* m_destination;
         double m_distance;
         RoadType m_type;
-        vector<Obstacle*> m_obstacles;
+        RoadCharacteristic m_characteristic;
+        std::vector<Obstacle*> m_obstacles;
     public:
-        Edge(Vertex* source, Vertex* destination, double distance, RoadType type)
-            : m_source(source), m_destination(destination), m_distance(distance), m_type(type) {}
+        Edge(Vertex* source, Vertex* destination, double distance,
+            RoadType type = RoadType::ROAD, RoadCharacteristic characteristic = RoadCharacteristic::STANDARD)
+            : m_source(source), m_destination(destination), m_distance(distance), m_type(type), m_characteristic(characteristic) {}
         virtual ~Edge() {
             for (auto* obstacle : m_obstacles) {
                 delete obstacle;
             }
         }
 
-    Vertex* getSource() const { return m_source; }
-    Vertex* getDestination() const { return m_destination; }
-    double getDistance() const { return m_distance; }
-    RoadType getType() const { return m_type; }
+        Vertex* getSource() const { return m_source; }
+        Vertex* getDestination() const { return m_destination; }
+        double getDistance() const { return m_distance; }
+        RoadType getType() const { return m_type; }
+        RoadCharacteristic getCharacteristic() const { return m_characteristic; }
 
-    void addObstacle(Obstacle* obstacle) { m_obstacles.push_back(obstacle); }
+        double calculateTravelTime(const Vehicle& vehicle) const {
+            double travel = vehicle.travelTime(m_distance, m_type, m_characteristic);
+            if (travel == std::numeric_limits<double>::infinity()) {
+                return travel;
+            }
+            double totalDelay = 0.0;
+            for (const auto* obstacle : m_obstacles) {
+                totalDelay += obstacle->delayHours;
+            }
+            return travel + totalDelay;
+        }
+
+        void addObstacle(Obstacle* obstacle) {
+            if(obstacle) m_obstacles.push_back(obstacle);
+        }
+
+        double getWeight(const Vehicle& vehicle) const {
+            return calculateTravelTime(vehicle);
+        }
 };
