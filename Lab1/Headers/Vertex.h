bf19fb5f10962db7cfb4dd3d2bf0e87519386bdd
anzhela <angela.tes3@gmail.com>
2025-10-07 23:55:15 +0300

    Changed destructor, deleted getToll() method

--- Lab1/Headers/Vertex.h
+++ Lab1/Headers/Vertex.h
@@ -12,20 +12,22 @@ class Vertex {
         std::string m_name;
         std::vector<Vehicle*> m_availableVehicles;
     protected:
-        void setId(int id) {
-            m_id = id;
-        }
+        void setId(int id) { m_id = id; }
     public:
-        Vertex() : m_name("untitled") {}
+        Vertex() : m_name() {}
         explicit Vertex(const std::string& name) : m_name(name) {}
-        virtual ~Vertex() = default;
+        virtual ~Vertex() {
+            for (auto* vehicle : m_availableVehicles) {
+                delete vehicle;
+            }
+            m_availableVehicles.clear();
+        }
 
         int getId() const { return m_id; }
-        std::string getName() const { return m_name; }
-        virtual double getToll() const { return 0.0; }
+        const std::string& getName() const { return m_name; }
 
-        void addVehicle(Vehicle* vehicle) { m_availableVehicles.push_back(vehicle); }
+        void addVehicle(Vehicle* vehicle) { if(vehicle) m_availableVehicles.push_back(vehicle); }
         const std::vector<Vehicle*>& getAvailableVehicles() const { return m_availableVehicles; }
 
-        template <typename TVertex, typename TEdge> friend class Graph;
+        template <typename TVertexT, typename TEdgeT> friend class Graph;
 };
