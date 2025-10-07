f2a147d1823864dbe01852e8b54dcb4f5cd1a725
anzhela <angela.tes3@gmail.com>
2025-10-07 23:49:14 +0300

    Expanded Obstacle structure, moved method implementations to Edge.cpp

--- Lab1/Headers/Edge.h
+++ Lab1/Headers/Edge.h
@@ -1,17 +1,17 @@
 #pragma once
 
-#include <limits>
 #include <string>
 #include <vector>
 #include "Vertex.h"
 #include "Vehicle.h"
-
-enum class RoadType { ROAD, RAIL, AIR, WATER };
-enum class RoadCharacteristic { HIGHWAY, CITY, DIRT_ROAD, PARK, STANDARD };
+#include "EnumClasses.h"
 
 struct Obstacle {
     std::string name;
     double delayHours = 0.0;
+    double speedMultiplier = 1.0;
+    ObstacleCause cause = ObstacleCause::CUSTOM_DELAY;
+    ObstacleImpact impact = ObstacleImpact::ALL_VEHICLES;
 };
 
 class Edge {
@@ -22,15 +22,15 @@ class Edge {
         RoadType m_type;
         RoadCharacteristic m_characteristic;
         std::vector<Obstacle*> m_obstacles;
+        static ObstacleImpact getImpact(RoadType type);
+
     public:
         Edge(Vertex* source, Vertex* destination, double distance,
-            RoadType type = RoadType::ROAD, RoadCharacteristic characteristic = RoadCharacteristic::STANDARD)
-            : m_source(source), m_destination(destination), m_distance(distance), m_type(type), m_characteristic(characteristic) {}
-        virtual ~Edge() {
-            for (auto* obstacle : m_obstacles) {
-                delete obstacle;
-            }
-        }
+            RoadType type = RoadType::ROAD, RoadCharacteristic characteristic = RoadCharacteristic::STANDARD);
+        virtual ~Edge();
+
+        void applyImpact(double& delay, double& speedFactor) const;
+        void addObstacle(Obstacle* obstacle);
 
         Vertex* getSource() const { return m_source; }
         Vertex* getDestination() const { return m_destination; }
@@ -38,23 +38,5 @@ class Edge {
         RoadType getType() const { return m_type; }
         RoadCharacteristic getCharacteristic() const { return m_characteristic; }
 
-        double calculateTravelTime(const Vehicle& vehicle) const {
-            double travel = vehicle.travelTime(m_distance, m_type, m_characteristic);
-            if (travel == std::numeric_limits<double>::infinity()) {
-                return travel;
-            }
-            double totalDelay = 0.0;
-            for (const auto* obstacle : m_obstacles) {
-                totalDelay += obstacle->delayHours;
-            }
-            return travel + totalDelay;
-        }
-
-        void addObstacle(Obstacle* obstacle) {
-            if(obstacle) m_obstacles.push_back(obstacle);
-        }
-
-        double getWeight(const Vehicle& vehicle) const {
-            return calculateTravelTime(vehicle);
-        }
+        double calculateTravelTime(const Vehicle& vehicle) const;
 };
