39ad22ec5744a07bb0ce43c643c01521e16e31e8
anzhela <angela.tes3@gmail.com>
2025-10-18 21:00:12 +0300

    Add corrected GraphAlgorithm.h (corrected isConnected method)

--- Lab1/Headers/GraphAlgorithm.h
+++ Lab1/Headers/GraphAlgorithm.h
@@ -7,8 +7,21 @@
 #include "Graph.h"
 #include "Vehicle.h"
 
+/**
+ * @class GraphAlgorithms
+ * @brief Static methods for common graph algorithms
+ */
 class GraphAlgorithms {
 public:
+    /**
+     * @brief Performs a Breadth-First Search starting from a given vertex ID
+     *
+     * @tparam TVertex The type of data stored in graph vertices
+     * @tparam TEdge The type of data stored on graph edges
+     * @param graph A constant reference to the graph to be traversed
+     * @param startId The ID of the starting vertex
+     * @return std::vector<int> A vector of vertex IDs in the order they were visited
+     */
     template <typename TVertex, typename TEdge>
     static std::vector<int> BFS(const Graph<TVertex, TEdge>& graph, int startId) {
         std::vector<int> order;
@@ -36,6 +49,15 @@ public:
         return order;
     }
 
+    /**
+     * @brief Performs a Depth-First Search starting from a given vertex ID
+     *
+     * @tparam TVertex The type of data stored in graph vertices
+     * @tparam TEdge The type of data stored on graph edges
+     * @param graph A constant reference to the graph to be traversed
+     * @param startId The ID of the starting vertex
+     * @return std::vector<int> A vector of vertex IDs in the order they were visited
+     */
     template <typename TVertex, typename TEdge>
     static std::vector<int> DFS(const Graph<TVertex, TEdge>& graph, int startId) {
         std::vector<int> order;
@@ -61,6 +83,16 @@ public:
         return order;
     }
 
+    /**
+     * @brief Implements Dijkstra's algorithm to find the shortest paths from a starting vertex
+     * @details Edge weight is calculated as the travel time using the specified vehicle
+     * @tparam TVertex The type of data stored in graph vertices
+     * @tparam TEdge The type of data stored on graph edges
+     * @param graph A reference to the graph
+     * @param startId The ID of the starting vertex
+     * @param vehicle A reference to the vehicle used for travel time calculation
+     * @return std::vector<double> A vector of the shortest "time" distances from startId to all other vertices
+     */
     template <typename TVertex, typename TEdge>
     static std::vector<double> Dijkstra(const Graph<TVertex, TEdge>& graph, int startId, const Vehicle& vehicle) {
         int n = graph.getNumberOfVertices();
@@ -111,6 +143,13 @@ public:
         return dist;
     }
 
+    /**
+     * @brief Checks if the graph is connected
+     * @tparam TVertex The type of data stored in graph vertices
+     * @tparam TEdge The type of data stored on graph edges
+     * @param graph A reference to the graph
+     * @return True if the graph is connected, false otherwise
+     */
     template <typename TVertex, typename TEdge>
     static bool isConnected(const Graph<TVertex, TEdge>& graph) {
         int n = graph.getNumberOfVertices();
@@ -146,11 +185,11 @@ public:
             int reachableCount = 1;
 
             while (!st2.empty()) {
-                int neighbor = st2.top(); st2.pop();
-                for (int current : graph.getNeighbors(neighbor)) {
-                    if (!visited[current]) {
-                        visited[current] = true;
-                        st2.push(current);
+                int current = st2.top(); st2.pop();
+                for (int neighbor : graph.getNeighbors(current)) {
+                    if (!visited[neighbor]) {
+                        visited[neighbor] = true;
+                        st2.push(neighbor);
                         reachableCount++;
                     }
                 }
